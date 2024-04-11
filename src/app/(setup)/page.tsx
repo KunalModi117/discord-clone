7b549c5d55e69edcb73ae9ce4ca83e9fca3b5d41
KunalModi117 <kunalmodi117@gmail.com>
2024-04-11 22:12:11 +0530

    initial modal for server

--- src/app/(setup)/page.tsx
+++ src/app/(setup)/page.tsx
@@ -1,3 +1,4 @@
+import { InitialModal } from "@/components/modals/InitialModal";
 import { db } from "@/lib/db";
 import { initialprofile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
@@ -16,7 +17,7 @@ const SetUpPage = async () => {
   if (server) {
     return redirect(`/servers/${server.id}`);
   }
-  return <div>Create a Server</div>;
+  return <InitialModal />;
 };
 
 export default SetUpPage;
