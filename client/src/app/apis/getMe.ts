9db92fe1f5a1634e8e85781a4d9eab2c1f4c4645
Kunal Modi <kunalm@webelight.co.in>
2025-08-10 17:58:11 +0530

    cookie change server side

--- client/src/app/apis/getMe.ts
+++ client/src/app/apis/getMe.ts
@@ -2,14 +2,17 @@ import { fetcher } from "@discord/utils/fetcher";
 import { cookies } from "next/headers";
 
 export const getMe = async () => {
-  const cookie = await cookies();
-  console.log(":cookie here - >: ", cookie, " :cookie ends:");
-  const token = cookie.get("token")?.value;
-  console.log(":token here - >: ", token, " :token ends:");
-  if (!token) return null;
+  const cookieStore = await cookies();
+  const cookiesString = cookieStore.toString();
+  console.log(":cookiesString here ->", cookiesString);
+
+  if (!cookiesString) {
+    return null;
+  }
 
   const response = await fetcher("/user/me", {
-    headers: { Cookie: `token=${token}` },
+    headers: { Cookie: cookiesString },
   });
+
   return response;
 };
